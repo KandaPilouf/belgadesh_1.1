8934dcc74b276f75e383d5f53329963fdb33b63f
KandaPilouf <samikhan199758@gmail.com>
2025-11-25 14:26:06 +0100

    added js animation for menu section

---
+++ main.js
@@ -0,0 +1,64 @@
+import gsap from "https://cdn.skypack.dev/gsap";
+import ScrollTrigger from "https://cdn.skypack.dev/gsap/ScrollTrigger";
+console.log(ScrollTrigger.version); // -> 3.12.2
+
+gsap.registerPlugin(ScrollTrigger);
+
+addEventListener("DOMContentLoaded", (event) => {
+  gsap.fromTo(
+    ".anim_fade_in_right_maki",
+    { x: -30, opacity: 0 },
+    {
+      scrollTrigger: {
+        trigger: ".coffee_menu__img",
+        start: "top, center",
+        markers: true,
+      },
+      x: 0,
+      opacity: 1,
+      duration: 1,
+    }
+  );
+
+  gsap.fromTo(
+    ".anim_fade_in_left_origan",
+    { x: 30, opacity: 0 },
+    {
+      scrollTrigger: {
+        trigger: ".bg-color--coffee",
+        start: "center, center ",
+      },
+      x: 0,
+      opacity: 1,
+      duration: 1,
+    }
+  );
+
+  gsap.fromTo(
+    ".anim_fade_in_right_text",
+    { x: 30, opacity: 0 },
+    {
+      scrollTrigger: {
+        trigger: ".coffee_menu__img",
+        start: "center, center ",
+      },
+      x: 0,
+      opacity: 1,
+      duration: 1,
+    }
+  );
+
+  gsap.fromTo(
+    ".anim_fade_in_left_text",
+    { x: -30, opacity: 0 },
+    {
+      scrollTrigger: {
+        trigger: ".bg-color--coffee",
+        start: "center, center ",
+      },
+      x: 0,
+      opacity: 1,
+      duration: 1,
+    }
+  );
+});
